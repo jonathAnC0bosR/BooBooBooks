1c1135b53c3c0e8e3bd2a7e827d069cccc3fe848
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-04-25 12:25:08 -0600

    Added code to the google api javascript, added the code that adds thumbnails of the books and the title of them

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@
 var topicInputEl = document.querySelector('#topic');
 var authorInputEl = document.querySelector('#author');
 var searchBtn = document.querySelector('#search');
+var booksCards = document.querySelector('.book-cards');
 
 
 // GLOBAL VAR 
@@ -11,6 +12,7 @@ var googleApiKey = 'AIzaSyBnclLTbT1mhObu7gIM7GD2zyWPGJMmCdA';
 
 var handleFormSubmit = function(event) {
     event.preventDefault();
+    booksCards.textContent = '';
     var author = authorInputEl.value.trim();
     var topic = topicInputEl.value.trim();
     if (author && topic) {
@@ -28,7 +30,7 @@ var getGoogleBooksInfo = function(author, topic) {
             if(response.ok) {
                 response.json()
                 .then(function(data){
-                    console.log(data)
+                    console.log(data);
                     displayBooksCards(data);
                 })
             } else {
@@ -49,9 +51,26 @@ var displayBooksCards = function(books) {
 
     let count = 0;
     for(var i = 0; i < books.items.length; i++) {
-        console.log(books.items[i]);
+        var bookCardEl = document.createElement('div');
         var bookTitle = books.items[i].volumeInfo.title;
+        var bookTitleEl = document.createElement('h1');
+        var bookImageLink = books.items[i].volumeInfo.imageLinks
+        var bookThumbnail = document.createElement('img');
+        bookTitleEl.textContent = bookTitle;
+        bookCardEl.appendChild(bookTitleEl);
         
+        if(bookImageLink) {
+            bookThumbnail.setAttribute('src', bookImageLink.smallThumbnail);
+            bookCardEl.appendChild(bookThumbnail);
+        } else {
+            console.log('No image you silly')
+            bookThumbnail.setAttribute('src', './assets/images/No_Image_Available.jpg');
+            bookCardEl.appendChild(bookThumbnail);
+        }
+        
+        booksCards.appendChild(bookCardEl);
+        
+        console.log(bookTitle);
         count ++;
         if (count === 5){
             break;
