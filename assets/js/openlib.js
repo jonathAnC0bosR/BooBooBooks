95f9ec1ea614b7a4db928cc36da629b179d9a569
Lizzz <alejandroraidi@gmail.com>
2023-04-24 19:19:02 -0600

    prints correct data

--- assets/js/openlib.js
+++ assets/js/openlib.js
@@ -5,7 +5,8 @@ has_fulltext: true
 */
 
 // This URL needs to be able to be changed by the user
-var requestUrl = 'https://openlibrary.org/search.json?title=the+lord+of+the+rings';
+autor = 'tolkien';
+var requestUrl = 'https://openlibrary.org/search.json?author='+autor;
 
 fetch(requestUrl)
     .then(function(response){
@@ -17,11 +18,14 @@ fetch(requestUrl)
         //This logs the first five results which are available to be borrowed and that have the full text
         //PROBLEM: SOLO LOGEA LOS PRIMEROS 5 ITEMS DEL ARRAY E LUGAR DE SEGUIR EVALUANDO HASTA REGRESAR 5 RESULTADOS
         //PROBLEM: ebook_access puede tener dos valores: borrowable y true
-        for(let i = 0; i < 5; i++){
-            let singleBook = data.docs[i];
-            if(singleBook.ebook_access == "borrowable" && singleBook.has_fulltext == true){
+        
+
+            for(let i = 0; i < data.docs.length; i++){
+                let singleBook = data.docs[i];
+    
+                if(singleBook.ebook_access == "borrowable" && singleBook.has_fulltext == true){
                 console.log(singleBook)
                 console.log('https://openlibrary.org' + singleBook.key);
-            }
-        }
+                }
+            } 
     });
