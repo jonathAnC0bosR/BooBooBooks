e1961fc8f2fac4f0e96cc36c950565b2b9d76d49
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-04-24 20:55:25 -0600

    Added code to the main javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,66 @@
 // DOM ELEMENTS
 
 var topicInputEl = document.querySelector('#topic');
-var kewordInputEl = document.querySelector('#keyword');
+var authorInputEl = document.querySelector('#author');
 var searchBtn = document.querySelector('#search');
+
 
 // GLOBAL VAR 
 
 var googleApiKey = 'AIzaSyBnclLTbT1mhObu7gIM7GD2zyWPGJMmCdA';
 
-addEventListener('click', function() {
-    var googleUrl = 'https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=' + googleApiKey;
-    fetch(googleUrl)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
+var handleFormSubmit = function(event) {
+    event.preventDefault();
+    var author = authorInputEl.value.trim();
+    var topic = topicInputEl.value.trim();
+    if (author && topic) {
+        getGoogleBooksInfo(author, topic)
+    } else {
+        alert('Please enter data')
+    }
+}
 
-})
+var getGoogleBooksInfo = function(author, topic) {
+    
+    var googleUrl = 'https://www.googleapis.com/books/v1/volumes?q=inauthor:' + author + '+subject:' + topic + '&key=' + googleApiKey;
+    fetch(googleUrl)
+        .then(function(response) {
+            if(response.ok) {
+                response.json()
+                .then(function(data){
+                    console.log(data)
+                    displayBooksCards(data);
+                })
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        }).catch(function(error) {
+            alert('Unable to connect to Google Books API')
+        })
+    
+}
+
+var displayBooksCards = function(books) {
+    
+    if (books.totalItems === 0) {
+        alert('Sorry, no books availabe');
+        return;
+    }
+
+    let count = 0;
+    for(var i = 0; i < books.items.length; i++) {
+        console.log(books.items[i]);
+        var bookTitle = books.items[i].volumeInfo.title;
+        
+        count ++;
+        if (count === 5){
+            break;
+        }
+    }
+    
+
+
+    
+}
+
+searchBtn.addEventListener('click', handleFormSubmit);
